01a0b3a4037a07ca8e2be61a66af340a23589307
kevin940726 <kevin830726@gmail.com>
2015-05-06 03:09:13 +0800

    Fix index.js

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ var express = require('./config/express');
 var google = require('google')
 var app = express();
 
-
 app.listen(process.env.PORT || port);
 module.exports = app;
-app.get('/',function(request, response){
-    response.end('Hello');
-});
 console.log('Server running at http://localhost:' + port);
